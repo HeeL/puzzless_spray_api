9e441e194fb2dff00718e55927ec2724a5571d00
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-07-27 20:37:14 +0200

    added listAll and findByUuid methods

--- src/main/scala/com/puzzless/models/Category.scala
+++ src/main/scala/com/puzzless/models/Category.scala
@@ -1,3 +1,16 @@
 package com.puzzless.models
 
+import com.puzzless.Db
+
 case class Category(uuid: String, title: String)
+
+object Category {
+
+  def findByUuid(uuid: String) = {
+    Db.query[Category].whereEqual("uuid", uuid).fetchOne().getOrElse(false).asInstanceOf[Category]
+  }
+
+  def listAll = {
+    Db.query[Category].fetch().asInstanceOf[Stream[Category]]
+  }
+}
