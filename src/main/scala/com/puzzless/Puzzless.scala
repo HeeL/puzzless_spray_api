f05033dbcb1a2df947674c8fce5cc2dec87a5e8b
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-08-02 23:31:04 +0200

    removed not relevant comments, reformat

--- src/main/scala/com/puzzless/Puzzless.scala
+++ src/main/scala/com/puzzless/Puzzless.scala
@@ -6,30 +6,20 @@ import spray.routing._
 import spray.http._
 import MediaTypes._
 import com.puzzless.models._
-
 import akka.actor.Props
 import spray.routing.HttpService
-
 import akka.pattern.ask
 import spray.routing.authentication.BasicAuth
 import scala.concurrent.duration._
 
-
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
 class PuzzlessActor extends Actor with HttpService {
 
-  // the HttpService trait defines only one abstract member, which
-  // connects the services environment to the enclosing actor or test
   def actorRefFactory = context
 
-  // this actor only runs our route, but you could add
-  // other things here, like request stream processing
-  // or timeout handling
   def receive = runRoute(puzzlessRoute)
 
   implicit val timeout = Timeout(5.seconds)
-  //implicit def executionContext = actorRefFactory.dispatcher
+
   import context.dispatcher
 
   val category = context.system.actorOf(Props[CategoryActor], "category")
