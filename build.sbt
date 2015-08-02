141a0263285ce9e6981d60191bbd55ad155e7117
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-08-02 22:17:25 +0200

    refactored json generation and categories crud

--- build.sbt
+++ build.sbt
@@ -10,15 +10,16 @@ libraryDependencies ++= {
   val akkaV = "2.3.9"
   val sprayV = "1.3.3"
   Seq(
-    "io.spray"            %%  "spray-can"     % sprayV,
-    "io.spray"            %%  "spray-routing" % sprayV,
-    "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
+    "io.spray"                     %%  "spray-can"           % sprayV,
+    "io.spray"                     %%  "spray-routing"       % sprayV,
+    "com.typesafe.akka"            %%  "akka-actor"          % akkaV,
+    "com.fasterxml.jackson.core"   %% "jackson-databind"     % "2.6.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.6.0",
-    "org.sorm-framework"  %   "sorm"          % "0.3.18",
-    "org.postgresql"      %   "postgresql"    % "9.4-1201-jdbc41",
-    "io.spray"            %%  "spray-testkit" % sprayV   % "test",
-    "com.typesafe.akka"   %%  "akka-testkit"  % akkaV    % "test",
-    "org.specs2"          %%  "specs2-core"   % "2.3.11" % "test"
+    "org.sorm-framework"           %   "sorm"                % "0.3.18",
+    "org.postgresql"               %   "postgresql"          % "9.4-1201-jdbc41",
+    "io.spray"                     %%  "spray-testkit"       % sprayV   % "test",
+    "com.typesafe.akka"            %%  "akka-testkit"        % akkaV    % "test",
+    "org.specs2"                   %%  "specs2-core"         % "2.3.11" % "test"
   )
 }
 
--- src/main/scala/com/puzzless/CategoryActor.scala
+++ src/main/scala/com/puzzless/CategoryActor.scala
@@ -1,37 +1,34 @@
 package com.puzzless
 
 import akka.actor.{Actor, ActorLogging}
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.puzzless.models.Category
 
 class CategoryActor extends Actor with ActorLogging {
 
-  val mapper = new OptionObjectMapper()
-  mapper.registerModule(DefaultScalaModule)
-
   def receive = {
     case "list" =>
       val categories = Category.listAll
-      val result = mapper.writeValueAsString(categories).asInstanceOf[Stream[Category]]
+      val result = JsonUtil.toJson(categories)
       sender ! result
 
     case ("show", uuid: String) =>
-      val category = Category.findByUuid(uuid).getOrElse(false).asInstanceOf[Category]
-      val result = mapper.writeValueAsString(category)
+      val category = Category.findByUuid(uuid)
+      val result = JsonUtil.toJson(category)
       sender ! result
 
     case ("create", title: String) =>
-      val category = Category.create(title).asInstanceOf[Category]
-      val result = mapper.writeValueAsString(category)
+      val category = Category.create(title)
+      val result = JsonUtil.toJson(category)
       sender ! result
 
     case ("update", uuid: String, title: String) =>
-      val category = Category.update(uuid, title).getOrElse(false).asInstanceOf[Category]
-      val result = mapper.writeValueAsString(category)
+      val category = Category.update(uuid, title)
+      val result = JsonUtil.toJson(category)
       sender ! result
 
     case ("delete", uuid: String) =>
-      val result = Category.delete(uuid).getOrElse("ERROR").toString.asInstanceOf[String]
+      val category = Category.delete(uuid)
+      val result = JsonUtil.toJson(category)
       sender ! result
   }
 
