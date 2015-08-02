4af8dec00d0b01c06838164991f6eb2b33fcdcdb
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-08-03 00:01:55 +0200

    reformat inline methods for category

--- src/main/scala/com/puzzless/models/Category.scala
+++ src/main/scala/com/puzzless/models/Category.scala
@@ -6,24 +6,14 @@ case class Category(uuid: String, title: String)
 
 object Category {
 
-  def findByUuid(uuid: String) = {
-    Db.query[Category].whereEqual("uuid", uuid).fetchOne()
-  }
+  def findByUuid(uuid: String) = Db.query[Category].whereEqual("uuid", uuid).fetchOne()
 
-  def listAll = {
-    Db.query[Category].fetch()
-  }
+  def listAll = Db.query[Category].fetch()
 
-  def create(title: String) = {
-    Db.save(Category(Db.uuid, title))
-  }
+  def create(title: String) = Db.save(Category(Db.uuid, title))
 
-  def update(uuid: String, title: String) = {
-    findByUuid(uuid).map(c => c.copy(title = title)).map(Db.save)
-  }
+  def update(uuid: String, title: String) = findByUuid(uuid).map(c => c.copy(title = title)).map(Db.save)
 
-  def delete(uuid: String) = {
-    findByUuid(uuid).map(c => Db.delete(c))
-  }
+  def delete(uuid: String) = findByUuid(uuid).map(c => Db.delete(c))
 
 }
